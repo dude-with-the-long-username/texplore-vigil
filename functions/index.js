7e9989f9c8995d5d263e5eda7aae2f3434e0e27e
Rajesh G <29516506+dude-with-the-long-username@users.noreply.github.com>
2021-10-25 01:12:01 +0530

    ✨ feat: Post todo using API

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,10 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
 exports.api = functions.https.onRequest(app);
