906ca5154f62ac75ec77980996d472e961b7a1b6
Rajesh G <29516506+dude-with-the-long-username@users.noreply.github.com>
2021-10-24 16:09:46 +0530

    ✨ feat: initial API communication (shows hello world on /todos route)

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,11 @@
+//index.js
+
 const functions = require('firebase-functions');
+const app = require('express')();
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//      response.send("Hello from Firebase!");
-// });
+const {
+    getAllTodos
+} = require('./APIs/todos')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
